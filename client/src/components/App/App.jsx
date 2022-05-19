97091c70fea95384cc8f50e42f79d1674c248406
Chipokon <velemshok1695@gmail.com>
2022-05-19 18:52:40 +0300

    slaveDone

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -4,13 +4,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { store as globalStore } from '../../redux/store'
 import { Provider } from 'react-redux'
 import Nav from '../Nav/Nav'
+import Home from '../Home/Home'
+import GameForm from '../GameForm/GameForm';
+
+
 function App() {
   return (
     <Provider store={globalStore}>
       <BrowserRouter>
         <Nav />
         <Routes>
-
+          <Route path='/' element={<Home />} />
+          <Route path='/gameForm' element = {<GameForm/>}/>
         </Routes>
       </BrowserRouter>
     </Provider>
